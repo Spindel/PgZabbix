f82aece6771b5d4953e6e618f8be61805c9b685b
D.S. Ljungmark <ljungmark@modio.se>
2017-10-20 19:55:46 +0200

    bugfix: Don't enter "None" as cache hit values
    Not supported by zabbix

--- pgzabbix/table.py
+++ pgzabbix/table.py
@@ -138,6 +138,8 @@ def psql_table_idx_cachehit_ratio(cur):
     out = "psql.table_idx_cachehit_ratio[{},{},{}]"
     cur.execute(query)
     for row in cur.fetchall():
+        if row[3] is None:
+            continue
         yield out.format(*row[:3]), row[3]
 
 
